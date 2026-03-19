fb14d408732aad021d69115d29b3f0497739dcff
Timofey Tanuki <timofey.tanuki@gmail.com>
2026-03-19 17:55:58 +0300

    Cleanup before committing a new version (upgrade to Tanuki.Atlyss 3.0.0)

--- PluginInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Tanuki.Atlyss.FluffUtilities;
-
-internal static class PluginInfo
-{
-    public const string
-        GUID = "cc8615a7-47a4-4321-be79-11e36887b64a",
-        Version = "{{VERSION}}";
-}
